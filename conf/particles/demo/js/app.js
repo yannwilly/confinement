a9f5638bdad83c781573a9ad58b793fca6a159bb
yannwilly <44833025+yannwilly@users.noreply.github.com>
2020-04-10 23:52:45 +0530

    Update app.js

--- conf/particles/demo/js/app.js
+++ conf/particles/demo/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value":5000,
+        "value":500,
         "density": {
           "enable": true,
           "value_area": 800
